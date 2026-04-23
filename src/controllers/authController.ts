3292f93c8c5eff265b477d9d5b577bdbb32cf2b9
Karangoyal2005 <goyalkarankg2005@gmail.com>
2026-04-23 19:39:24 -0400

    add validation for duplicate user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,6 +7,10 @@ import jwt from "jsonwebtoken";
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = new User({
