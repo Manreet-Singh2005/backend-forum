8869a0beb0a1ea18b6af81496dcfbc8e3a2b54b8
Parampreet Singh <Parampreetsingh6413@conestogac.on.ca>
2026-04-24 10:00:41 -0400

    Made changes for test running

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes";
 import postRoutes from "./routes/postRoutes";
 import commentRoutes from "./routes/commentRoutes";
-import adminRoutes from "./routes/adminRoutes";  
+import adminRoutes from "./routes/adminRoutes";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Forum API is running ");
@@ -21,11 +19,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/api/admin", adminRoutes);    
+app.use("/api/admin", adminRoutes);
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URI!)
+mongoose
+  .connect(process.env.MONGO_URI!)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 export default app;
