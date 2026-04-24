7e8f8ed5d8d45612c38e56fa0091bebc2c59ce18
Manreet Singh <sandhu.manreet05@gmail.com>
2026-04-23 22:27:09 -0400

    Done: Bruno Testing

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -10,10 +10,13 @@ const postSchema = new mongoose.Schema(
       ref: "User",
       required: true
     },
-    likes:{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
   },
   { timestamps: true } 
 );
